483ecd9053fa8870ad4526f7a9e1ae1cf840689b
Daniel Zhang <d.zhang9@share.epsb.ca>
2022-08-27 17:55:45 -0600

    Added fps counter to top window, added new font (Segoe UI) for non sudoku text.

--- display.py
+++ display.py
@@ -2,19 +2,23 @@
 Sudoku Board Display with pygame
 date: 2022-08-17
 """
-
 import pygame
 
 
 class Display:
     """
-    class to create display using pygame
-
+    sudoku display made using pygame
     Attributes:
-        BOARD: (list)
+        BOARD: (list) of (tuple): (character (str), colour (tuple))
         WIDTH: (int)
         HEIGHT: (int)
         FPS: (int)
+        DIMENSIONS: (tuple)
+        CLOCK: (pygame.time.Clock)
+        SCREEN: (pygame.Surface)
+        BACKGROUND: (pygame.Surface)
+        NFONT: (pygame.Font) font used for numbers
+        MFONT: font used for misc purposes
 
     Methods:
         update(): (none)
@@ -26,24 +30,36 @@ class Display:
         get_board(): (string)
     """
 
-    def __init__(self, size, fps):
+    def __init__(self, size, fps, caption):
+        """
+        :param size: (int) pixel size of sudoku board
+        :param fps: (int)
+        :param caption: (str)
+        """
         self.__FPS = fps
         self.__WIDTH = size
         self.__HEIGHT = size
         self.__DIMENSIONS = (self.__WIDTH, self.__HEIGHT)
-        self.__FRAME = pygame.time.Clock()
+        self.__CLOCK = pygame.time.Clock()
         self.__SCREEN = pygame.display.set_mode(self.__DIMENSIONS)
-        pygame.display.set_caption("Sudoku Solver")
+        pygame.display.set_caption(caption)
         self.__BACKGROUND = pygame.Surface(self.__DIMENSIONS, pygame.SRCALPHA, 32)
-        self.__BGCOLOUR = (230, 230, 230)
-        self.__BACKGROUND.fill(self.__BGCOLOUR)
+        self.__BACKGROUND.fill((230, 230, 230))
         self.__BOARD = [list('000000000') for _ in range(9)]
         pygame.font.init()
-        self.__FONT = pygame.font.SysFont('franklingothicmedium', 70)
+        self.__NFONT = pygame.font.SysFont('franklingothicmedium', 70)
+        self.__MFONT = pygame.font.SysFont('Segoe UI', 14)
 
     # --- MODIFIERS --- #
 
     def update(self, restrict=True):
+        """
+        Updates the entire screen
+        to be honest, due to the number of updates needed and the requirement to go asap,
+        this code should be optimized quite a bit to not update the entire screen
+        :param restrict: (bool) whether the fps should be unlimited
+        :return:
+        """
         self.__SCREEN.blit(self.__BACKGROUND, (0, 0))  # clears screen
 
         for i in range(1, 9):  # draw lines
@@ -56,19 +72,25 @@ class Display:
             pygame.draw.line(self.__SCREEN, (0, 0, 0), (self.__WIDTH, self.__HEIGHT / 9 * i),
                              (0, self.__HEIGHT / 9 * i), width)
 
-        for y, row in enumerate(self.__BOARD):
+        for y, row in enumerate(self.__BOARD):  # draw board
             for x, val in enumerate(row):
                 if val[0] == '0':
                     pass
                 else:
                     try:
-                        text = self.__FONT.render(val[0], True, val[1])
+                        text = self.__NFONT.render(val[0], True, val[1])
                         self.__SCREEN.blit(text, (self.__WIDTH / 9 * x + self.__WIDTH / 36, self.__HEIGHT / 9 * y))
                     except TypeError:
                         pass
 
-        if restrict:
-            self.__FRAME.tick(self.__FPS)
+        fps_text = self.__MFONT.render("FPS: " + str(int(self.__CLOCK.get_fps())), True, (10, 10, 10))
+
+        self.__SCREEN.blit(fps_text, (10, 0))
+
+        if restrict:  # during solving we don't want fps <= 30
+            self.__CLOCK.tick(self.__FPS)
+        else:
+            self.__CLOCK.tick()
         pygame.display.flip()  # updates screen
 
     def edit_board(self, x, y, val, pixels=True):
